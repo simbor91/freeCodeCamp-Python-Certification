5d9698ec3fe5d12da5787c98338c03e85173ec1a
Simone <simobnovantuno@gmail.com>
2026-02-09 23:27:42 +0100

    add: code for exercise 019 polygon area calculator

--- 06-oop/018build-player-interface.py
+++ 06-oop/018build-player-interface.py
@@ -6,12 +6,12 @@ from abc import ABC, abstractmethod
 import random
 
 class Player(ABC):
-    def __init__(self):
+    def __init__(self) -> None:
         self.moves = []
         self.position = (0,0)
         self.path = [self.position]
 
-    def make_move(self):
+    def make_move(self) -> tuple:
         new_move = random.choice(self.moves)
         x, y = self.position
         dx, dy = new_move
@@ -29,11 +29,11 @@ class Player(ABC):
 # Dice: "Io so come muovermi (logica in make_move), ma non so quali siano le direzioni possibili finché 
 # non mi dici che tipo di giocatore sono".
 class Pawn(Player):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.moves = [(0,1), (0,-1), (-1,0), (1,0)] # up, down, left, right
 
-    def level_up(self):
+    def level_up(self) -> None:
         self.moves += [(1,1), (1,-1), (-1,-1), (-1,1)] # adding more moves to the moves attribute, representing the four diagonal movements (for example, 1 unit down plus 1 unit left)
 
 p = Pawn()
