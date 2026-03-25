e92c527a030fb7de50e9010ad853a8a88cc2b6e1
Simone <simobnovantuno@gmail.com>
2026-03-25 16:34:23 +0100

    create: bread-first search algo

---
+++ 09-graphs-trees/031breadth-first-search.py
@@ -0,0 +1,25 @@
+# Graphs and Trees
+# 031 Workshop: Implement the Breadth First Search Algorithm
+
+def gen_parentheses(pairs):
+    if not isinstance(pairs, int):
+        return 'The number of pairs should be an integer'
+    if pairs < 1:
+        return 'The number of pairs should be at least 1'
+    
+    queue = [('', 0, 0)]
+    result = []
+    while queue:
+        # print(queue)
+        current, opens_used, closes_used = queue.pop(0) # unpacking the tuple
+        if len(current) == 2 * pairs:
+            result.append(current)
+        else:
+            if opens_used < pairs:
+                queue.append((current + '(', opens_used + 1, closes_used))
+            if closes_used < opens_used:
+                queue.append((current + ')', opens_used, closes_used + 1))
+            
+    return result
+
+print(gen_parentheses(3))
